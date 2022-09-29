6e4a432bcd4dc2156b92f4fe0a975a526d99d318
Pfeffer <botond.pfeffer@gmail.com>
2022-09-29 12:02:29 +0200

    majdnem kesz

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './style.css';
+import './node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import './node_modules/bootstrap/dist/css/bootstrap.css'
 
+function darkMode() {
+    var element = document.body;
+
+    element.classList.toggle("dark-mode");
+ }
+
+ 
+document.getElementById("darkMode").addEventListener("click", darkMode);
+
+document.getElementById("darkMode").addEventListener("click", darkMode);
+
+document.getElementById("kepszelesseg").addEventListener("keyup", kepUrl);
+
+document.getElementById("kepkeretvastagsag").addEventListener("keyup", kepUrl);
+
+document.getElementById("kepkeretszine").addEventListener("keyup", kepUrl);
+
+document.addEventListener('DOMContentLoaded', init)
